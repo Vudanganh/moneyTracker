6c9a7018727288b3665ca268b4bfc9400f3c9775
Vu Dang Anh <vudang@web.de>
2021-05-13 12:48:15 +0200

    create startscreen and extend tabbar with a watchlist

--- moneyTracker/Controller/StartScreenController.swift
+++ moneyTracker/Controller/StartScreenController.swift
@@ -6,7 +6,55 @@
 //
 
 import UIKit
+import Lottie
 
-class StartScreenController {
+class StartScreenController: UIViewController {
     
+    
+    @IBOutlet weak var animationView: AnimationView!
+    
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        
+        // choose animation
+        
+        let animationToShow = Animation.named("ARCube")
+        animationView.animation = animationToShow
+        
+        // Set animation content mode
+        
+        animationView.contentMode = .scaleAspectFit
+        
+        // Set animation loop mode
+        
+        animationView.loopMode = .loop
+        
+        // Adjust animation speed
+        
+        animationView.animationSpeed = 0.5
+        
+        // set backgroundcolor
+        
+        animationView.backgroundColor = UIColor(named: "Background")
+        
+        
+        // 4. Play animation
+        animationView.play()
+        
+        endStartscreen()
+        
+    }
+    
+    
+    func endStartscreen () {
+        let timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { (timer) in
+            
+            self.performSegue(withIdentifier: "startScreenToHomeScreen", sender: self)
+        }
+        
+    }
 }
